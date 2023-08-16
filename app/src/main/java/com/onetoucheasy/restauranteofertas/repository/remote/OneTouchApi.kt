3e858a93dc9e22d79fad3bb9434ef6ca0f4bb770
CamilaLopez <camilallopez95@gmail.com>
2023-08-16 16:56:38 +0200

    changes api

--- app/src/main/java/com/onetoucheasy/restauranteofertas/repository/remote/OneTouchApi.kt
+++ app/src/main/java/com/onetoucheasy/restauranteofertas/repository/remote/OneTouchApi.kt
@@ -2,11 +2,9 @@ package com.onetoucheasy.restauranteofertas.repository.remote
 
 import retrofit2.http.GET
 import retrofit2.http.Header
-import retrofit2.http.Headers
 
 //TODO Refactor login method
 interface OneTouchApi {
-
     @GET("auth/signIn")
     suspend fun performLogin(@Header("CDS-ApiKey") apiKey: String, @Header("Authorization") loginData: String): String
 }
