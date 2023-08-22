6a41a1b1079615782432a8c4428a8029528d9eba
Marta Maquedano <marta.maquedano@gmail.com>
2023-08-22 18:19:25 +0200

    - Composable SearchBar created.

--- app/src/main/java/com/onetoucheasy/restauranteofertas/ui/scaffolds/MainScreen.kt
+++ app/src/main/java/com/onetoucheasy/restauranteofertas/ui/scaffolds/MainScreen.kt
@@ -1,9 +1,32 @@
 package com.onetoucheasy.restauranteofertas.ui.scaffolds
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
-import com.onetoucheasy.restauranteofertas.ui.viewModels.LoginViewModel
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.onetoucheasy.restauranteofertas.R
+import com.onetoucheasy.restauranteofertas.ui.theme.White
 import com.onetoucheasy.restauranteofertas.ui.viewModels.MainScreenViewModel
 
 @Composable
@@ -20,4 +43,46 @@ fun MainScreenContent() {
 @Composable
 fun MainScreen_Preview() {
     MainScreenContent()
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(text: String,
+                 screenWidth: Dp,
+                 visible: Boolean = true,
+                 onValueChange: (String) -> (Unit))
+{
+    TextField(value = text,
+        onValueChange = onValueChange,
+        modifier = Modifier
+            .width(screenWidth * 4.0f / 5)
+            .background(White)
+            .padding(5.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .border(
+                BorderStroke(
+                    0.5.dp,
+                    Color.Black
+                ), RoundedCornerShape(8.dp)
+            ),
+        trailingIcon = {
+            Icon(Icons.Default.Search, contentDescription  = "Búsqueda", tint = Color.Black)
+        },
+        placeholder = {
+                FormLabel(hint = stringResource(R.string.main_view_search_bar_hint), TextDecoration.None)
+        },
+        visualTransformation = if (true){
+            PasswordVisualTransformation()
+        }else{
+            VisualTransformation.None},
+        singleLine = true,
+        colors = TextFieldDefaults.textFieldColors(containerColor = Color.White)
+    )
+}
+
+@Preview
+@Composable
+fun SearchBar_Preview() {
+    SearchBar("", screenWidth = 500.dp ,true , { _->Unit})
 }
